e53d71a4e60e3c966f461c5b07e80c71898f4efb
zakiasegond <segondzakia@live.fr>
2018-07-09 14:59:58 +0200

    fonction supp en cours

--- annuaire.js
+++ annuaire.js
@@ -2,37 +2,19 @@
 
 
 
-var repertoire = JSON.parse( localStorage.getItem('objet') );
+var repertoire = JSON.parse( localStorage.getItem("objet") );
 
 for (var i = 0; i < repertoire.length ; i++) {
-	$('#body').append("<tr><td>" + repertoire[i].nom + "</td><td>" + repertoire[i].prenom + "</td><td>" + repertoire[i].tel + "</td><td>" + repertoire[i].ville + "</td><td>" + repertoire[i].age + "</td></tr>");
+	$('#body').append("<tr><td>" + repertoire[i].nom + "</td><td>" + repertoire[i].prenom + "</td><td>" + repertoire[i].tel + "</td><td>" + repertoire[i].ville + "</td><td>" + repertoire[i].age + "</td><td><button id='modif' type='button' class='btn btn-success' data-toggle='modal' data-target='#modifff'>M</button></td><td><button id='supp' type='button' class='btn btn-danger' data-toggle='modal' data-target='#supppp'>S</button></td></tr>");
 	
 }
 
 
-function display() {
 
-
-	// body...
-	var new_array_json = localStorage.getItem("object"); //recupére la chaine de caractere dans le local
-	var get_array = JSON.parse(new_array_json); //retransforme la chaine de caractere en tableau (du langage JSON au JS)
-	
-	for (var i = 0; i < get_array.length ; i++)
-
-	{
-
-		$('#body').append("<tr><td>" + get_array[i].nom + "</td><td>" + get_array[i].prenom + "</td><td>" + get_array[i].tel + "</td><td>" + get_array[i].ville + "</td><td>" + get_array[i].age + "</td></tr>");
-	
-	}
-}
-
-
-var new_array = [];
-var cmpt = 0;
 
 $("#Ajouter").click(function()
 {
-	cmpt++;
+	
 
 	var new_nom = $("#a1").val();
 	var new_prenom = $("#a2").val();
@@ -50,7 +32,10 @@ $("#Ajouter").click(function()
 	  age: new_age, 
 	}
 
-	new_array.push(array);
+	$('#body').append("<tr><td>" + new_nom + "</td><td>" + new_prenom + "</td><td>" + new_tel + "</td><td>" + new_ville + "</td><td>" + new_age + "</td><td><button id='modif' type='button' class='btn btn-success' data-toggle='modal' data-target='#modifff'>M</button></td><td><button id='supp' type='button' class='btn btn-danger' data-toggle='modal' data-target='#supppp'>S</button></td></tr>");
+	
+
+	repertoire.push(array);
 	
 	$("#ajouttt").modal("hide");
 	new_nom = $("#a1").val('');
@@ -59,23 +44,38 @@ $("#Ajouter").click(function()
 	new_ville = $("#a4").val('');
 	new_age = $("#a5").val('');
 	
-	var add = cmpt.toString();
+	
 
-	console.log(cmpt)
-	var new_array_json = JSON.stringify(new_array); //transforme le tableau en chaine de caractere (du langage JS au JSON)
-	localStorage.setItem("object" ,new_array_json); //envois la chaine créer dans le local storage
+	
+	var repertoire_json = JSON.stringify(repertoire); //transforme le tableau en chaine de caractere (du langage JS au JSON)
+	localStorage.setItem("objet" ,repertoire_json); //envois la chaine créer dans le local storage
 	
 	
 
-display()
+
+});
+ 
+$("#ok").click(function(){
+
+
+	var rechercher = $('#rechercher').val();
+
+	$('tbody tr').hide();
+	$('table tr:contains('+rechercher+')').show();
+});
+
+
+
+
+$('table tbody').delegate('supp', function(){
+	var tableau = $(this).data('tableau');
+
+	supprimer_contact(tableau);
 
 });
 
-display()
 
 
-
-//affichage tableau en dur dans le navigateur
 
 
 
